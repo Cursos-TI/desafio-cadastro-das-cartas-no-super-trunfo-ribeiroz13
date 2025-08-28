dab01f8e5733cf4a8d26641d1010217eaa2ba295
jeferson ribeiro <128777436+ribeiroz13@users.noreply.github.com>
2025-08-28 18:51:38 +0000

    correção Desafio Super Trunfo novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
 int main() {
+    char codigo1[4], codigo2[4];
     int populacao1, populacao2;
     float area1, area2;
     float pib1, pib2;
     int pontos1, pontos2;
 
+    // Cadastro da priimeira carta
     printf("Cadastro da primeira carta:\n");
+    printf("Código da carta (ex.: A01): ");
+    scanf("%s", codigo1);
+
     printf("População: ");
     scanf("%d", &populacao1);
 
@@ -19,7 +24,11 @@ int main() {
     printf("Número de pontos turísticos: ");
     scanf("%d", &pontos1);
 
+    // Cadastro da segunda carta
     printf("\nCadastro da segunda carta:\n");
+    printf("Código da carta (ex.: B02): ");
+    scanf("%s", codigo2);
+
     printf("População: ");
     scanf("%d", &populacao2);
 
@@ -32,13 +41,16 @@ int main() {
     printf("Número de pontos turísticos: ");
     scanf("%d", &pontos2);
 
-    printf("\n Dados da Primeira Carta \n");
+    // dados cadastrados
+    printf("\nDados da Primeira Carta\n");
+    printf("Código: %s\n", codigo1);
     printf("População: %d\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões\n", pib1);
     printf("Pontos Turísticos: %d\n", pontos1);
 
-    printf("\n Dados da Segunda Carta \n");
+    printf("\nDados da Segunda Carta\n");
+    printf("Código: %s\n", codigo2);
     printf("População: %d\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões\n", pib2);
@@ -46,6 +58,7 @@ int main() {
 
     return 0;
 }
+
 
 
 
